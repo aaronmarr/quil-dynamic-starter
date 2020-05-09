6dba3b1322e679cb5814328d7950f7782af1bd70
Aaron Marr <aaronmarr@protonmail.com>
2020-05-09 14:06:36 +0100

    Initial commit

---
+++ src/quil_workflow/dynamic.clj
@@ -0,0 +1,17 @@
+(ns quil-workflow.dynamic
+  (:require [quil.core :as q]))
+
+(defn setup []
+  (q/smooth)
+  (q/frame-rate 1)
+  (q/background 200))
+
+(defn draw []
+  (q/stroke (q/random 255) (q/random 255) (q/random 255))
+  (q/stroke-weight (q/random 10))
+  (q/fill (q/random 255))
+
+  (let [diam (q/random 100)
+        x    (q/random (q/width))
+        y    (q/random (q/height))]
+    (q/ellipse x y diam diam)))
